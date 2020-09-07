33ddb3846d21f32019c38f91221a676811897ff8
gavinsimon1 <gavin.simon808@gmail.com>
2020-09-06 15:11:05 -1000

    updated navbar

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('hey!')
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
